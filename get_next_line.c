da9bc80bd78eae3e9c270b92a29f083873073623
Beach Linnea <blinnea@at-l8.21-school.ru>
2019-09-28 17:22:50 +0300

    something strange

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,13 @@
 /*   By: blinnea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 15:30:40 by blinnea           #+#    #+#             */
-/*   Updated: 2019/09/21 17:23:37 by blinnea          ###   ########.fr       */
+/*   Updated: 2019/09/28 17:18:25 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-t_list	*find_file(t_list *head, int fd)
+t_list	*find_fd(t_list *head, const int fd)
 {
 	while (head)
 	{
@@ -24,51 +23,92 @@ t_list	*find_file(t_list *head, int fd)
 	return (NULL);
 }
 
-char	*ft_strextend(char **s1, const char *s2)
+t_list	*add_fd(t_list **head, const int fd)
 {
-	char	*res;
+	t_list	*new;
 
-	if (!(res = ft_strjoin(*s1, s2)))
+	if (!(new = ft_lstnew("\0", (size_t)fd)))
 		return (NULL);
-	ft_strdel(s1);
-	return (res);
+	ft_lstadd(head, new);
+	return (new);
+}
+
+void	crutch(void *s, size_t size)
+{
+	ft_memdel(&s);
+	size = 0;
+}
+
+size_t	get_strlist(const int fd, char *content, t_list **strlist)
+{
+	int		len;
+	char	buff[BUFF_SIZE + 1];
+	t_list	*ptr;
+
+	if (!(*strlist = ft_lstnew(content, ft_strlen(content))))
+		return (0);
+	ptr = *strlist;
+	while ((len = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[len] = 0;
+		if (!(ptr->next = ft_lstnew(ft_strdup(buff), len)))
+		{
+			ft_lstdel(strlist, &crutch);
+			return (0);
+		}
+		ptr = ptr->next;
+		if (ft_strchr(buff, '\n'))
+			break;
+	}
+	if (*strlist == ptr)
+		return ((*strlist)->content_size);
+	if (*strlist + 1 == ptr)
+		return ((*strlist)->content_size + ptr->content_size);
+	return ((*strlist - ptr - 2) * BUFF_SIZE + \
+			(*strlist)->content_size + ptr->content_size);
+}
+
+t_list	*strlist_to_line(t_list *ptr, char *line, size_t l_line)
+{
+	char	*newline;
+
+	while (ptr->next)
+	{
+		ft_strlcat(line, ptr->content, l_line);
+		ptr = ptr->next;
+	}
+	if ((newline = ft_strchr(ptr->content, '\n')))
+		*newline++ = 0;
+	ft_strlcat(line, ptr->content, l_line);
+	ptr->content = newline;
+	return (ptr);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	char			buff[BUFF_SIZE + 1];
 	static t_list	*flist;
 	t_list			*curr;
-	int				len;
-	char			*crloc;
+	char			*newline;
+	t_list			*strlist;
+	size_t			l_strlist;
 
-	crloc = NULL;
-	if (fd < 0 || !line || read(fd, buff, 0) < 0 || !(*line = ft_strnew(1)))
+	if (!(curr = find_fd(flist, fd)) && !(curr = add_fd(&flist, fd)))
 		return (ERR);
-	if (!(curr = find_file(flist, fd)))
+	if ((newline = ft_strchr(curr->content, '\n')))
 	{
-		if (!(curr = ft_lstnew("\0", (size_t)fd)))
-			return (ERR);
-		ft_lstadd(&flist, curr);
+		*newline = 0;
+		*line = ft_strdup(curr->content);
+		curr->content = newline + 1;
+		return (OK);
 	}
-	printf("%s\n", curr->content);
-	*line = ft_strextend(line, curr->content);
+	l_strlist = get_strlist(fd, curr->content, &strlist);
 	free(curr->content);
-	while ((len = read(fd, buff, BUFF_SIZE)))
-	{
-		buff[len] = 0;
-		if ((crloc = ft_strchr(buff, '\n')))
-			break;
-		if (!(*line = ft_strextend(line, buff)))
-			return (ERR);
-	}
-	if (len < BUFF_SIZE && !ft_strlen(*line))
+	if (!(*line = ft_strnew(l_strlist)))
+		return (ERR);
+	curr->content = ft_strdup(strlist_to_line(strlist, *line, \
+				l_strlist)->content);
+	ft_lstdel(&strlist, &crutch);
+	if (!curr->content)
 		return (END);
-	if (crloc)
-	{
-		*crloc++ = 0;
-		*line = ft_strextend(line, buff);
-	}
-	curr->content = ft_strdup(crloc);
 	return (OK);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jrameau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 05:26:42 by jrameau           #+#    #+#             */
-/*   Updated: 2019/09/21 17:20:24 by blinnea          ###   ########.fr       */
+/*   Updated: 2019/09/28 17:21:36 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 
 int		main(int argc, char **argv)
 {
-		int		fd;
-			char	*line;
+	int		fd;
+	char	*line;
 
-				if (argc == 1)
-							fd = 0;
-					else if (argc == 2)
-								fd = open(argv[1], O_RDONLY);
-						else
-									return (2);
-							while (get_next_line(fd, &line) == 1)
-									{
-												//ft_putendl(line);
-														free(line);
-															}
-								if (argc == 2)
-											close(fd);
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
+	if (argc == 2)
+		close(fd);
+	return (0);
 }
